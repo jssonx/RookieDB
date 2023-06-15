2c824024de04fbf34b59d955cc2f2e074ff8ccc0
jsn <yx546@cornell.edu>
2023-06-14 21:41:58 -0400

    feat: finish join algorithms - grace hash join

--- src/main/java/edu/berkeley/cs186/database/query/join/GHJOperator.java
+++ src/main/java/edu/berkeley/cs186/database/query/join/GHJOperator.java
@@ -71,6 +71,15 @@ public class GHJOperator extends JoinOperator {
         // You may find the implementation in SHJOperator.java to be a good
         // starting point. You can use the static method HashFunc.hashDataBox
         // to get a hash value.
+        for (Record record : records) {
+            DataBox columnValue = left ? record.getValue(getLeftColumnIndex()) : record.getValue(getRightColumnIndex());
+            int hash = HashFunc.hashDataBox(columnValue, pass);
+            int partitionNum = hash % partitions.length;
+            if (partitionNum < 0) {
+                partitionNum += partitions.length;
+            }
+            partitions[partitionNum].add(record);
+        }
         return;
     }
 
@@ -112,6 +121,33 @@ public class GHJOperator extends JoinOperator {
         // You shouldn't refer to any variable starting with "left" or "right"
         // here, use the "build" and "probe" variables we set up for you.
         // Check out how SHJOperator implements this function if you feel stuck.
+        
+        // 1. build：构建哈希表：拉链法
+        // age
+        // age = 1: 小红 小黄
+        // age = 2
+        Map<DataBox, List<Record>> hashTable = new HashMap<>();
+        for (Record record : buildRecords) {
+            DataBox buildJoinValue = record.getValue(buildColumnIndex);
+            if (!hashTable.containsKey(buildJoinValue)) {
+                hashTable.put(buildJoinValue, new ArrayList<>());
+            }
+            hashTable.get(buildJoinValue).add(record);
+        }
+        
+        // 2. probe：遍历probeRecords，找到匹配的记录
+        for (Record probeRecord : probeRecords) {
+            DataBox probeJoinValue = probeRecord.getValue(probeColumnIndex);
+            if (!hashTable.containsKey(probeJoinValue)) {
+                continue;
+            }
+            // 
+            for (Record buildRecord : hashTable.get(probeJoinValue)) {
+                Record concatRecord =
+                    probeFirst ? probeRecord.concat(buildRecord) : buildRecord.concat(probeRecord);
+                this.joinedRecords.add(concatRecord);
+            }
+        }
     }
 
     /**
@@ -136,6 +172,11 @@ public class GHJOperator extends JoinOperator {
             // TODO(proj3_part1): implement the rest of grace hash join
             // If you meet the conditions to run the build and probe you should
             // do so immediately. Otherwise you should make a recursive call.
+            if (leftPartitions[i].getNumPages() <= this.numBuffers - 2 || rightPartitions[i].getNumPages() <= this.numBuffers - 2) {
+                buildAndProbe(leftPartitions[i], rightPartitions[i]);
+            } else {
+                this.run(leftPartitions[i], rightPartitions[i], pass + 1);
+            }
         }
     }
 
@@ -203,6 +244,18 @@ public class GHJOperator extends JoinOperator {
 
         // TODO(proj3_part1): populate leftRecords and rightRecords such that
         // SHJ breaks when trying to join them but not GHJ
+
+        // partition数：B-1 buffers
+        // pageNum数  ：<= B-2 buffers 
+        // page记录数： <= 8个 
+        // 总数：partition数 * partition中的pageNum最大数 * page记录数 + 1 = 161个记录
+        // 注意：由于数据倾斜，会在更少的记录下SHJ就崩溃了 %8
+        // [ 1 9 17 25] 
+
+        for (int i = 0; i < 161; i++) {
+            leftRecords.add(createRecord(i));
+            rightRecords.add(createRecord(i));
+        }
         return new Pair<>(leftRecords, rightRecords);
     }
 
@@ -223,6 +276,13 @@ public class GHJOperator extends JoinOperator {
         ArrayList<Record> leftRecords = new ArrayList<>();
         ArrayList<Record> rightRecords = new ArrayList<>();
         // TODO(proj3_part1): populate leftRecords and rightRecords such that GHJ breaks
+
+        //一个partition最多容纳（B-2）* 8个记录
+        // So with (B-2) * 8 + 1 = 33 records, GHJ will fail
+        for (int i = 0; i < 33; i++) {
+            leftRecords.add(createRecord(0));
+            rightRecords.add(createRecord(0));
+        }
 
         return new Pair<>(leftRecords, rightRecords);
     }
